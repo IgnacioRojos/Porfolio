431362cd6a51c555b4a46fb2cba3908359b9cc1c
IgnacioRojos <nachorojos99@gmail.com>
2025-07-08 10:26:29 -0300

    se agregaron mejoras en el css y el html, ademas se agregaron nuevos elementos para descargar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const btnSwitch = document.querySelector("#switch");
 
-btnSwitch.addEventListener("click", ()=>{
-    document.body.classList.toggle("dark");
-    btnSwitch.classList.toggle("active");
+// Al cargar la página, verifica si estaba activado el modo oscuro
+if (localStorage.getItem("modo-oscuro") === "true") {
+  document.body.classList.add("dark");
+  btnSwitch.classList.add("active");
+}
+
+btnSwitch.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  btnSwitch.classList.toggle("active");
+
+  // Guarda el estado actual en localStorage
+  const modoOscuroActivo = document.body.classList.contains("dark");
+  localStorage.setItem("modo-oscuro", modoOscuroActivo);
 });
